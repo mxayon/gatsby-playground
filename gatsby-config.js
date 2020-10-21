404f811d084740ab2b21ebdd99df0b5fb448a6c8
mxayon <mxnkpl@gmail.com>
2020-10-20 20:33:55 -0700

    takesover barebones config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+    siteMetadata: {
+        title: 'Mx Gatsby Spin',
+    },
+    plugins: ['gatsby-plugin-react-helmet'],
 }

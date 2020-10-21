aca776b8041a3f59e4dee8272f7e35e69f38fd5f
mxayon <mxnkpl@gmail.com>
2020-10-20 20:37:01 -0700

    first changes on the page1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+const IndexPage = () => ( <
+    div >
+    <
+    h1 > Learning to < br / > design and code React apps < /h1> <
+    p > Complete courses about the best tools and design systems.Prototype and build apps with React and Swift. < /p>  <
+    p > Now go build something great. < /p>  <
+    Link to = "/page-2/" > Go to page 2 < /Link>  <
+    /div>
 )
 
 export default IndexPage

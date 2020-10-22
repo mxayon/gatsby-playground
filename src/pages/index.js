265c2607984a60bad3b9f4fbba12b42783e396bc
mxayon <mxnkpl@gmail.com>
2020-10-21 18:33:44 -0700

    starts first group of components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => ( <
-    div >
-    <
-    h1 > Learning to < br / > design and code React apps < /h1> <
-    p > Complete courses about the best tools and design systems.Prototype and build apps with React and Swift. < /p>  <
-    p > Now go build something great. < /p>  <
-    Link to = "/page-2/" > Go to page 2 < /Link>  <
-    /div>
+const IndexPage = () => (
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>
+          {' '}
+          Learning to <br /> design and code React apps{' '}
+        </h1>
+        <p>
+          {' '}
+          Complete courses about the best tools and design systems.Prototype and
+          build apps with React and Swift.{' '}
+        </p>
+        <p> Now go build something great. </p>
+        <Link to="/page-2/"> Go to page 2 </Link>
+      </div>
+    </div>
+  </div>
 )
 
 export default IndexPage

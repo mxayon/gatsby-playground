8f6d24e37c87ef96c821249145cd5d5a2113f8b3
mxayon <mxnkpl@gmail.com>
2020-10-25 12:24:43 -0700

    card components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Cards'
 
 const IndexPage = () => (
   <div>
@@ -44,6 +45,25 @@ const IndexPage = () => (
           </path>
         </svg>
       </div>
+    </div>
+    <div className="Cards">
+      <h2>11 done, more on the way...</h2>
+      <Card 
+        title="React for Designers"
+        text="12 sections"
+        image={require('../images/wallpaper.jpg')}/>
+      <Card 
+        title="Course II"
+        text="12 sections"
+        image={require('../images/wallpaper2.jpg')}/>
+      <Card 
+        title="Course III"
+        text="12 sections"
+        image={require('../images/wallpaper3.jpg')}/>
+      <Card 
+        title="Course IV"
+        text="12 sections"
+        image={require('../images/wallpaper4.jpg')}/>
     </div>
   </div>
 )

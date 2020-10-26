6624e40026d1b170b442feaabb21e5361764a83e
mxayon <mxnkpl@gmail.com>
2020-10-25 21:05:39 -0700

    adds media queries and wave height

--- src/components/Section.js
+++ src/components/Section.js
@@ -9,6 +9,11 @@ const SectionGroup = styled.div`
     display: grid;
     grid-template-rows: 300px auto;
     grid-gap: 20px;
+    position: relative;
+
+    @media (max-width: 64opx) {
+        height: 820px;
+    }
 `
 
 const SectionLogo = styled.img`
@@ -24,7 +29,7 @@ const SectionTitleGroup = styled.div`
     grid-gap: 20px;
     grid-template-rows: auto 100%;
 
-    @media(max-width: 720px) {
+    @media(max-width: 720px){
         grid-template-columns: 1fr;
     }
 `
@@ -34,21 +39,42 @@ const SectionTitle = styled.h3`
     font-size: 60px;
     margin: 0;
     line-height: 1.2;
+
+    @media (max-width: 720px){
+        font-size: 40px;
+    }
 `
 
 const SectionText = styled.p`
   color: #ffffff;
 `
 
+const WaveBottom = styled.div`
+    position: absolute;
+    width: 100%
+    bottom: 0;
+    bottom: -6px;
+`
+
+const WaveTop = styled.div`
+    position: absolute;
+    width: 100%;
+    top: -6px;
+    transform: rotate(180deg);
+`
+
 const Section = props => (
-    <SectionGroup image={props.image}>
-        <Wave />
-        <SectionLogo src={props.logo} />
-        <SectionTitleGroup>
-            <SectionTitle>{props.title}</SectionTitle>
-            <SectionText>{props.text}</SectionText>
-        </SectionTitleGroup>
-    </SectionGroup>
-);
+  <SectionGroup image={props.image}>
+    <WaveTop><Wave /></WaveTop>
+    <WaveBottom>
+      <Wave />
+    </WaveBottom>
+    <SectionLogo src={props.logo} />
+    <SectionTitleGroup>
+      <SectionTitle>{props.title}</SectionTitle>
+      <SectionText>{props.text}</SectionText>
+    </SectionTitleGroup>
+  </SectionGroup>
+)
 
 export default Section
